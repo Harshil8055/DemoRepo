21df4b3eb1140a2e42318f0d8be7bda69ba1475d
Jain Harshil <jain.harshil@BLRETV-WWXG9K74.local>
2022-07-22 10:28:35 +0530

    First commit

---
+++ src/DemoApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author jain.harshil
+ *
+ */
+public class DemoApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my demo application");
+
+	}
+
+}
